11ee10be0f291d82abe6d7bcf9015e4a816cf28a
cesarvargasjr <cesar_vargas_jr@hotmail.com>
2022-01-08 20:45:53 -0300

    Desafio Firedev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import './styles/App.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Menu from "./components/layout/Menu";
+import axios from "axios";
+
+/* import api from "./services/api"; */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://alemdocodigo.herokuapp.com/list_posts")
+            .then((response) => {
+                setPosts(response.data.posts)
+            }).catch(() => {
+                console.log("Erro!")
+            })
+    }, [])
+
+    return (
+        <div /* className="App" */>
+            <Router>
+                <Menu />
+            </Router>
+            <h1 className="Title">Animes mais populares</h1>
+            <div className="Catalago">
+                {posts.map((post, key) => {
+                    return (
+                        <div className="Card" key={key}>
+                            <div className="Card-body" >
+                                <h1>{post.title}</h1>
+                                <div className="Line"></div>
+                                <h2>{post.content}</h2>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,89 @@
+import './Menu.css'
+import React from 'react'
+
+/* import { Link } from 'react-router-dom' */
+
+const Menu = props => (
+
+    <nav className="Menu">
+        <ul>
+            <li>
+                <a href=""><img src="logoFiredev.svg" /></a>
+            </li>
+            <li>
+                <a href="">ANIME</a>
+            </li>
+            <li>
+                <a href="">MANGA</a>
+            </li>
+        </ul>
+        <div className="div">
+            <input type="text" className="Input" />
+            <a href="" className="IconPesquisa">
+                <img src="iconPesquisa.svg" />
+            </a>
+            <a href="" className="IconFavoritos" >
+                <img src="logoFavoritos.svg" />
+            </a>
+        </div>
+    </nav>
+)
+
+export default Menu
+
+
+
+{/* <nav className="Menu">
+        <ul>
+            <li>
+                <a href=""><img src="logoFiredev.svg" /></a>
+            </li>
+            <li>
+                <a href="">ANIME</a>
+            </li>
+            <li>
+                <a href="">MANGA</a>
+            </li>
+            <li>
+                <a href="">
+                    <input type="text" className="Input"></input>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="logoFavoritos.svg" />
+                </a>
+            </li>
+        </ul>
+    </nav> */}
+
+
+
+
+{/* <aside className="Menu">
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">
+                        <img src="logoFiredev.svg" />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">ANIME</Link>
+                </li>
+                <li>
+                    <Link to="/">MANGA</Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <input type="text" className="Input"></input>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/">
+                        <img src="logoFavoritos.svg" />
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    </aside> */}
